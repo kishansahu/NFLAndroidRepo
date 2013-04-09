1f1cd9b67d4c3e7982f9930b80006ed2c42a9199
qainfotech <qainfotech@DLLA.(none)>
2013-04-09 17:12:12 +0530

    committing game activity sub part

--- Fragments/src/com/example/fragments/GameActivity.java
+++ Fragments/src/com/example/fragments/GameActivity.java
@@ -9,30 +9,34 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GameActivity extends Activity{
-
+public class GameActivity extends Activity {
 
 	FragmentManager fragmentManager;
-	Fragment maninMenuFragment;
+	Fragment mainMenuFragment;
 	FragmentTransaction ft;
 	ImageButton sliderButton;
-    TextView headerTextView;
-    ImageButton closeButtonHeader;
-    
+	ImageButton closeButtonHeader;
+	TextView headerTextView;
+	boolean showSlider;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 		fragmentManager = getFragmentManager();
-		maninMenuFragment = fragmentManager.findFragmentById(R.id.topicMenuFragment);
-		headerTextView = (TextView)findViewById(R.id.menuHeader);
-        headerTextView.setText("LiveClips");
-        closeButtonHeader = (ImageButton)findViewById(R.id.closeButtonHeader);
-        closeButtonHeader.setVisibility(View.VISIBLE);
+		mainMenuFragment = fragmentManager
+				.findFragmentById(R.id.topicMenuFragment);
+		headerTextView = (TextView) findViewById(R.id.menuHeader);
+		headerTextView.setText("LiveClips");
+		closeButtonHeader = (ImageButton) findViewById(R.id.closeButtonHeader);
+		closeButtonHeader.setVisibility(View.VISIBLE);
+		closeButtonHeader.setOnClickListener(closeButtonListener);
+
 	}
 
 	@Override
@@ -49,22 +53,52 @@ public class GameActivity extends Activity{
 		case R.id.sliderButton:
 			performSliderAction();
 			return true;
-		/*case R.id.help:
-			showHelp();
-			return true;*/
+			/*
+			 * case R.id.help: showHelp(); return true;
+			 */
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
 
 	private void performSliderAction() {
-		
+
+		/*
+		 * ft = fragmentManager.beginTransaction();
+		 * 
+		 * if (maninMenuFragment.isVisible()) {
+		 * 
+		 * ft.hide(maninMenuFragment);
+		 * 
+		 * 
+		 * Toast.makeText(AppContentActivity.this, "button clicked visible",
+		 * Toast.LENGTH_SHORT) .show();
+		 * 
+		 * 
+		 * } else {
+		 * 
+		 * ft.show(maninMenuFragment);
+		 * 
+		 * ft.addToBackStack(null);
+		 * 
+		 * }
+		 * 
+		 * ft.commit();
+		 */
+
+	};
+
+	private OnClickListener closeButtonListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
 			ft = fragmentManager.beginTransaction();
 
-			if (maninMenuFragment.isVisible()) {
+			if (mainMenuFragment.isVisible()) {
 
-				ft.hide(maninMenuFragment);
-
+				ft.hide(mainMenuFragment);
+				showSlider = true;
+				invalidateOptionsMenu();
 				/*
 				 * Toast.makeText(AppContentActivity.this,
 				 * "button clicked visible", Toast.LENGTH_SHORT) .show();
@@ -72,7 +106,7 @@ public class GameActivity extends Activity{
 
 			} else {
 
-				ft.show(maninMenuFragment);
+				ft.show(mainMenuFragment);
 
 				ft.addToBackStack(null);
 
@@ -80,8 +114,15 @@ public class GameActivity extends Activity{
 
 			ft.commit();
 
-		
+		}
+
 	};
 
-
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (showSlider) {
+			MenuItem menuItem = menu.add(0, Menu.FIRST, 0, "Slider");
+			menuItem.setIcon(R.drawable.pic4);
+		}
+		return super.onPrepareOptionsMenu(menu);
+	};
 }
